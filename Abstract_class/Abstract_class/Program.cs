cb12d2604013141b3ecde68fc8d8456233208258
mahimaverma7 <mahima@mail.fresnostate.edu>
2016-09-20 19:26:19 -0700

    Add files via upload

---
+++ Abstract_class/Abstract_class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstract_class
+{
+
+       abstract class ShapesClass
+        {
+            abstract public int Area();
+        }
+        class rectangle : ShapesClass
+        {
+              int side1 = 0;
+              int side2 = 0;
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="x">This is Length</param>
+            /// <param name="y">This is Breadth</param>
+                public rectangle(int x, int y)
+                {
+                     side1 = x;
+                     side2 = y;
+                 }
+    
+                 public override int Area()
+                 {
+                    return side1 * side2;
+                    }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           rectangle rec = new rectangle(9,3);
+           Console.WriteLine("Area of the Rectangle = {0}", rec.Area());
+           Console.ReadLine();
+        }
+    }
+}
